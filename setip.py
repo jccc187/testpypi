87f28bed354fcbcd89f5957e34be94dd205d5a39
ccerd <948805722@qq.com>
2020-07-22 10:39:39 +0800

    Add files via upload

---
+++ setip.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+setup(
+    name='uitestool',
+    version='1.0.0',
+    description='a automated test tools',
+    author='yanchaochao',
+    author_email='yanchaochao@xiaomi.com',
+    url='https://github.com/szmgithubcode/',
+    #packages=find_packages(),
+    packages=['autotest'],
+    #install_requires=['requests'],
+)
